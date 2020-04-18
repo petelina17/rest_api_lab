18c580ed7f67d8b90bbaf70ca0893497690dc18a
petelina17 <petelina17@gmail.com>
2020-04-18 16:29:58 +0200

    expres init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json('HELLO')
+})
+
+
+
+
+
+
+
+
+app.listen(3000, () => console.log('Server is running'))
+
