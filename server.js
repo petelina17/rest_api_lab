33ab766a1efcb60d078eea242f8509124440bd2b
petelina17 <petelina17@gmail.com>
2020-04-18 17:19:50 +0200

    endpoints, /products is working

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require('fs')
+
 
 const app = express()
 
@@ -8,11 +10,35 @@ app.get('/', (req, res) => {
   res.json('HELLO')
 })
 
+// read/get the product
+app.get('/product', (req, res) => {
 
+})
 
+// delete product
+app.del('/product', (req, res) => {
 
+})
 
+//create/add new product
+app.post('/product', (req, res) => {
 
+})
+
+// edit/update product
+app.put('/product', (req, res) => {
+
+})
+
+// read/get all products from product-data.json
+app.get('/products', (req, res) => {
+  fs.readFile('product-data.json', 'utf8', function (err, data) {
+    // server fills respons params
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(data)
+  })
+})
 
 
 app.listen(3000, () => console.log('Server is running'))
